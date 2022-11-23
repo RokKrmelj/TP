3b42a6650f9a18a8d5ade4a3708e11885724d3c6
RokKrmelj <rk1499@student.uni-lj.si>
2022-11-23 10:34:25 +0100

    domača naloga 4

--- DN4/naloga3.py
+++ DN4/naloga3.py
@@ -1,5 +1,5 @@
 import requests
 
 iskalniNiz = input("Vpiši iskalni niz: ")
-iskanje = requests.get('https://www.rtvslo.si/iskalnik?q=' + str(iskalniNiz))
+iskanje = requests.get('https://www.rtvslo.si/iskalnik?q=' + iskalniNiz)
 print(iskanje.text)
